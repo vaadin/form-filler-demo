c8066bf0c94afa5e076f3907063a4c9cec8adf1c
Mikhail Shabarov <mikhail@vaadin.com>
2023-08-15 16:46:50 +0300

    add demo views

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "formfillerdemo")
+@Theme(value = "my-theme")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/application/data/OrderItem.java
@@ -0,0 +1,49 @@
+package com.example.application.data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class OrderItem implements Serializable {
+    private String orderId;
+    private String itemName;
+    private LocalDate orderDate;
+    private String orderStatus;
+    private Double orderCost;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Double getOrderTotal() {
+        return orderCost;
+    }
+
+    public void setOrderTotal(Double orderCost) {
+        this.orderCost = orderCost;
+    }
+
+    public String getOrderId() { return orderId; }
+
+    public void setOrderId(String orderId) { this.orderId = orderId; }
+
+}
